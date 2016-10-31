6536a974db64f7a54f79825a40b74897648ba616
ashbigmass <ashbigmass@gmail.com>
2016-10-31 10:32:57 +0900

    psr

--- public_html/config/config.inc.php
+++ public_html/config/config.inc.php
@@ -1,34 +1,10 @@
 <?php
-/* Copyright (C) NAVER <http://www.navercorp.com> */
+if(version_compare(PHP_VERSION, '5.4.0', '<')) @error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_WARNING);
+else @error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_WARNING ^ E_STRICT);
 
-/**
- * set the include of the class file and other environment configurations
- *
- * @file   config/config.inc.php
- * @author NAVER (developers@xpressengine.com)
- */
-if(version_compare(PHP_VERSION, '5.4.0', '<'))
-{
-	@error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_WARNING);
-}
-else
-{
-	@error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_WARNING ^ E_STRICT);
-}
+if(!defined('__XE__')) exit();
 
-if(!defined('__XE__'))
-{
-	exit();
-}
-
-/**
- * @deprecated __ZBXE__ will be removed. Use __XE__ instead.
- */
 define('__ZBXE__', __XE__);
-
-/**
- * Display XE's full version.
- */
 define('__XE_VERSION__', '1.8.26');
 define('__XE_VERSION_ALPHA__', (stripos(__XE_VERSION__, 'alpha') !== false));
 define('__XE_VERSION_BETA__', (stripos(__XE_VERSION__, 'beta') !== false));
@@ -38,272 +14,46 @@ define('__XE_VERSION_STABLE__', (!__XE_VERSION_ALPHA__ && !__XE_VERSION_BETA__ &
 define('__XE_MIN_PHP_VERSION__', '5.3.0');
 define('__XE_RECOMMEND_PHP_VERSION__', '5.5.0');
 
-/**
- * @deprecated __ZBXE_VERSION__ will be removed. Use __XE_VERSION__ instead.
- */
 define('__ZBXE_VERSION__', __XE_VERSION__);
-
-/**
- * The base path to where you installed zbXE Wanted
- */
 define('_XE_PATH_', str_replace('config/config.inc.php', '', str_replace('\\', '/', __FILE__)));
-
 
 // Set can use other method instead cookie to store session id(for file upload)
 ini_set('session.use_only_cookies', 0);
 
-
-if(file_exists(_XE_PATH_ . 'config/package.inc.php'))
-{
-	require _XE_PATH_ . 'config/package.inc.php';
-}
-else
-{
-	/**
-	 * Package type
-	 */
+if(file_exists(_XE_PATH_ . 'config/package.inc.php')) require _XE_PATH_ . 'config/package.inc.php';
+else {
 	define('_XE_PACKAGE_', 'XE');
-
-	/**
-	 * Location
-	 */
 	define('_XE_LOCATION_', 'en');
-
-	/**
-	 * Location site
-	 */
 	define('_XE_LOCATION_SITE_', 'http://www.xpressengine.com/');
-
-	/**
-	 * Download server
-	 */
 	define('_XE_DOWNLOAD_SERVER_', 'http://download.xpressengine.com/');
 }
 
-/*
- * user configuration files which override the default settings
- * save the following information into config/config.user.inc.php
- * <?php
- * define('__DEBUG__', 0);
- * define('__DEBUG_OUTPUT__', 0);
- * define('__DEBUG_PROTECT__', 1);
- * define('__DEBUG_PROTECT_IP__', '127.0.0.1');
- * define('__DEBUG_DB_OUTPUT__', 0);
- * define('__LOG_SLOW_QUERY__', 0);
- * define('__LOG_SLOW_TRIGGER__', 0);
- * define('__LOG_SLOW_ADDON__', 0);
- * define('__LOG_SLOW_WIDGET__', 0);
- * define('__OB_GZHANDLER_ENABLE__', 1);
- * define('__ENABLE_PHPUNIT_TEST__', 0);
- * define('__PROXY_SERVER__', 'http://domain:port/path');
- */
-if(file_exists(_XE_PATH_ . 'config/config.user.inc.php'))
-{
-	require _XE_PATH_ . 'config/config.user.inc.php';
-}
+if(file_exists(_XE_PATH_ . 'config/config.user.inc.php')) require _XE_PATH_ . 'config/config.user.inc.php';
 
-if(!defined('__DEBUG__'))
-{
-	/**
-	 * output debug message(bit value)
-	 *
-	 * <pre>
-	 * 0: generate debug messages/not display
-	 * 1: display messages through debugPrint() function
-	 * 2: output execute time, Request/Response info
-	 * 4: output DB query history
-	 * </pre>
-	 */
-	define('__DEBUG__', 0);
-}
+if(!defined('__DEBUG__')) define('__DEBUG__', 0);
+if(!defined('__DEBUG_OUTPUT__')) define('__DEBUG_OUTPUT__', 0);
+if(!defined('__DEBUG_PROTECT__')) define('__DEBUG_PROTECT__', 1);
+if(!defined('__DEBUG_PROTECT_IP__')) define('__DEBUG_PROTECT_IP__', '127.0.0.1');
+if(!defined('__DEBUG_DB_OUTPUT__')) define('__DEBUG_DB_OUTPUT__', 0);
+if(!defined('__LOG_SLOW_QUERY__')) define('__LOG_SLOW_QUERY__', 0);
+if(!defined('__LOG_SLOW_TRIGGER__')) define('__LOG_SLOW_TRIGGER__', 0);
+if(!defined('__LOG_SLOW_ADDON__')) define('__LOG_SLOW_ADDON__', 0);
+if(!defined('__LOG_SLOW_WIDGET__')) define('__LOG_SLOW_WIDGET__', 0);
+if(!defined('__DEBUG_QUERY__')) define('__DEBUG_QUERY__', 0);
+if(!defined('__OB_GZHANDLER_ENABLE__')) define('__OB_GZHANDLER_ENABLE__', 1);
+if(!defined('__ENABLE_PHPUNIT_TEST__')) define('__ENABLE_PHPUNIT_TEST__', 0);
+if(!defined('__PROXY_SERVER__')) define('__PROXY_SERVER__', NULL);
+if(!defined('__ERROR_LOG__')) define('__ERROR_LOG__', 0);
 
-if(!defined('__DEBUG_OUTPUT__'))
-{
-	/**
-	 * output location of debug message
-	 *
-	 * <pre>
-	 * 0: connect to the files/_debug_message.php and output
-	 * 1: HTML output as a comment on the bottom (when response method is the HTML)
-	 * 2: Firebug console output (PHP 4 & 5. Firebug/FirePHP plug-in required)
-	 * </pre>
-	 */
-	define('__DEBUG_OUTPUT__', 0);
-}
+if((__DEBUG_OUTPUT__ == 2) && version_compare(PHP_VERSION, '6.0.0') === -1) require _XE_PATH_ . 'libs/FirePHPCore/FirePHP.class.php';
 
-if(!defined('__DEBUG_PROTECT__'))
-{
-	/**
-	 * output comments of the firePHP console and browser
-	 *
-	 * <pre>
-	 * 0: No limit (not recommended)
-	 * 1: Allow only specified IP addresses
-	 * </pre>
-	 */
-	define('__DEBUG_PROTECT__', 1);
-}
-
-if(!defined('__DEBUG_PROTECT_IP__'))
-{
-	/**
-	 * Set a ip address to allow debug
-	 */
-	define('__DEBUG_PROTECT_IP__', '127.0.0.1');
-}
-
-if(!defined('__DEBUG_DB_OUTPUT__'))
-{
-	/**
-	 * DB error message definition
-	 *
-	 * <pre>
-	 * 0: No output
-	 * 1: files/_debug_db_query.php connected to the output
-	 * </pre>
-	 */
-	define('__DEBUG_DB_OUTPUT__', 0);
-}
-
-if(!defined('__LOG_SLOW_QUERY__'))
-{
-	/**
-	 * Query log for only timeout query among DB queries
-	 *
-	 * <pre>
-	 * 0: Do not leave a log
-	 * = 0: leave a log when the slow query takes over specified seconds
-	 * Log file is saved as ./files/_slowlog_query.php file
-	 * </pre>
-	 */
-	define('__LOG_SLOW_QUERY__', 0);
-}
-
-if(!defined('__LOG_SLOW_TRIGGER__'))
-{
-	/**
-	 * Trigger excute time log
-	 *
-	 * <pre>
-	 * 0: Do not leave a log
-	 * > 0: leave a log when the trigger takes over specified milliseconds
-	 * Log file is saved as ./files/_slowlog_trigger.php
-	 * </pre>
-	 */
-	define('__LOG_SLOW_TRIGGER__', 0);
-}
-
-if(!defined('__LOG_SLOW_ADDON__'))
-{
-	/**
-	 * Addon excute time log
-	 *
-	 * <pre>
-	 * 0: Do not leave a log
-	 * > 0: leave a log when the trigger takes over specified milliseconds
-	 * Log file is saved as ./files/_slowlog_addon.php
-	 * </pre>
-	 */
-	define('__LOG_SLOW_ADDON__', 0);
-}
-
-if(!defined('__LOG_SLOW_WIDGET__'))
-{
-	/**
-	 * Widget excute time log
-	 *
-	 * <pre>
-	 * 0: Do not leave a log
-	 * > 0: leave a log when the widget takes over specified milliseconds
-	 * Log file is saved as ./files/_slowlog_widget.php
-	 * </pre>
-	 */
-	define('__LOG_SLOW_WIDGET__', 0);
-}
-
-if(!defined('__DEBUG_QUERY__'))
-{
-	/**
-	 * Leave DB query information
-	 *
-	 * <pre>
-	 * 0: Do not add information to the query
-	 * 1: Comment the XML Query ID
-	 * </pre>
-	 */
-	define('__DEBUG_QUERY__', 0);
-}
-
-if(!defined('__OB_GZHANDLER_ENABLE__'))
-{
-	/**
-	 * option to enable/disable a compression feature using ob_gzhandler
-	 *
-	 * <pre>
-	 * 0: Not used
-	 * 1: Enabled
-	 * Only particular servers may have a problem in IE browser when sending a compression
-	 * </pre>
-	 */
-	define('__OB_GZHANDLER_ENABLE__', 1);
-}
-
-if(!defined('__ENABLE_PHPUNIT_TEST__'))
-{
-	/**
-	 * decide to use/not use the php unit test (Path/tests/index.php)
-	 *
-	 * <pre>
-	 * 0: Not used
-	 * 1: Enabled
-	 * </pre>
-	 */
-	define('__ENABLE_PHPUNIT_TEST__', 0);
-}
-
-if(!defined('__PROXY_SERVER__'))
-{
-	/**
-	 * __PROXY_SERVER__ has server information to request to the external through the target server
-	 * FileHandler:: getRemoteResource uses the constant
-	 */
-	define('__PROXY_SERVER__', NULL);
-}
-
-if(!defined('__ERROR_LOG__'))
-{
-	/**
-	 * __ERROR_LOG__ 는 PHP의 에러로그를 출력하는 기능입니다. 개발시 워닝에러이상의 에러부터 잡기 시작합니다.
-	 *
-	 * 0: 사용하지 않음
-	 * 1: 사용함
-	 */
-	define('__ERROR_LOG__', 0);
-}
-
-// Require specific files when using Firebug console output
-if((__DEBUG_OUTPUT__ == 2) && version_compare(PHP_VERSION, '6.0.0') === -1)
-{
-	require _XE_PATH_ . 'libs/FirePHPCore/FirePHP.class.php';
-}
-
-// Set Timezone as server time
-if(version_compare(PHP_VERSION, '5.3.0') >= 0)
-{
-	date_default_timezone_set(@date_default_timezone_get());
-}
+if(version_compare(PHP_VERSION, '5.3.0') >= 0) date_default_timezone_set(@date_default_timezone_get());
 
 // Require a function-defined-file for simple use
 require(_XE_PATH_ . 'config/func.inc.php');
 
-if(__DEBUG__) {
-	define('__StartTime__', getMicroTime());
-}
-
-if(__DEBUG__) {
-	$GLOBALS['__elapsed_class_load__'] = 0;
-}
-
+if(__DEBUG__) define('__StartTime__', getMicroTime());
+if(__DEBUG__) $GLOBALS['__elapsed_class_load__'] = 0;
 $GLOBALS['__xe_autoload_file_map'] = array_change_key_case(array(
 	'CacheBase' => 'classes/cache/CacheHandler.class.php',
 	'CacheHandler' => 'classes/cache/CacheHandler.class.php',
@@ -400,18 +150,14 @@ $GLOBALS['__xe_autoload_file_map'] = array_change_key_case(array(
 	'TablesTag' => 'classes/xml/xmlquery/tags/table/TablesTag.class.php',
 ), CASE_LOWER);
 
-function __xe_autoload($class_name)
-{
+function __xe_autoload($class_name) {
 	if(__DEBUG__) {
 		$time_at = getMicroTime();
 	}
 
-	if(isset($GLOBALS['__xe_autoload_file_map'][strtolower($class_name)]))
-	{
+	if(isset($GLOBALS['__xe_autoload_file_map'][strtolower($class_name)])) {
 		require _XE_PATH_ . $GLOBALS['__xe_autoload_file_map'][strtolower($class_name)];
-	}
-	elseif(preg_match('/^([a-zA-Z0-9_]+?)(Admin)?(View|Controller|Model|Api|Wap|Mobile)?$/', $class_name, $matches))
-	{
+	} elseif(preg_match('/^([a-zA-Z0-9_]+?)(Admin)?(View|Controller|Model|Api|Wap|Mobile)?$/', $class_name, $matches)) {
 		$candidate_filename = array();
 		$candidate_filename[] = 'modules/' . $matches[1] . '/' . $matches[1];
 		if(isset($matches[2]) && $matches[2]) $candidate_filename[] = 'admin';
@@ -420,8 +166,7 @@ function __xe_autoload($class_name)
 
 		$candidate_filename = implode('.', $candidate_filename);
 
-		if(file_exists(_XE_PATH_ . $candidate_filename))
-		{
+		if(file_exists(_XE_PATH_ . $candidate_filename)) {
 			require _XE_PATH_ . $candidate_filename;
 		}
 	}
@@ -430,10 +175,9 @@ function __xe_autoload($class_name)
 		$GLOBALS['__elapsed_class_load__'] += getMicroTime() - $time_at;
 	}
 }
+
 spl_autoload_register('__xe_autoload');
 
 if(file_exists(_XE_PATH_  . '/vendor/autoload.php')) {
 	require _XE_PATH_  . '/vendor/autoload.php';
 }
-/* End of file config.inc.php */
-/* Location: ./config/config.inc.php */
