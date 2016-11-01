16089847e4f6cead9dae765e4b55878a36d09bd6
lemontreeyk <lemontreeyk@gmail.com>
2016-11-01 14:34:41 +0900

    파일에 로드파일, 로드디렉토리 등 변수로 만들어 주세요 좀

--- public_html/addons/resize_image/resize_image.addon.php
+++ public_html/addons/resize_image/resize_image.addon.php
@@ -14,14 +14,10 @@ if(!defined('__XE__'))
 
 $LF[0] = './addons/resize_image/css/resize_image.mobile.css';
 $LF[1] = './addons/resize_image/js/resize_image.min.js';
-if($called_position == 'after_module_proc' && Context::getResponseMethod() == "HTML" && !isCrawler())
-{
-	if(Mobile::isFromMobilePhone())
-	{
+if($called_position == 'after_module_proc' && Context::getResponseMethod() == "HTML" && !isCrawler()) {
+	if(Mobile::isFromMobilePhone()) {
 		Context::loadFile($LF[0], true);
-	}
-	else
-	{
+	} else {
 		Context::loadJavascriptPlugin('ui');
 		Context::loadFile(array($LF[1], 'body', '', null), true);
 	}
