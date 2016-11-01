834c34056302fb1ff98205b73b331a9c0ca1df5c
ashbigmass <ashbigmass@gmail.com>
2016-11-01 11:21:43 +0900

    금일 작업
    1. 세션관련 필요없는 함수 recompileCache 삭제했습니다.
    2. 세션 컨트롤 클래스 필요없어 삭제했는데 복구해야 하는지 말씀해 주세요

--- public_html/modules/session/session.class.php
+++ public_html/modules/session/session.class.php
@@ -8,32 +8,27 @@
  *
  * The session management class
  */
-class session extends ModuleObject
-{
+class session extends ModuleObject {
 	var $lifetime = 18000;
 	var $session_started = false;
 
-	function session()
-	{
+	function session() {
 		if(Context::isInstalled()) $this->session_started= true;
 	}
 
 	/**
 	 * @brief Additional tasks required to accomplish during the installation
 	 */
-	function moduleInstall()
-	{
+	function moduleInstall() {
 		$oDB = &DB::getInstance();
 		$oDB->addIndex("session","idx_session_update_mid", array("member_srl","last_update","cur_mid"));
-
 		return new Object();
 	}
 
 	/**
 	 * @brief A method to check if the installation has been successful
 	 */
-	function checkUpdate()
-	{
+	function checkUpdate() {
 		$oDB = &DB::getInstance();
 		if(!$oDB->isTableExists('session')) return true;
 		if(!$oDB->isColumnExists("session","cur_mid")) return true;
@@ -44,23 +39,18 @@ class session extends ModuleObject
 	/**
 	 * @brief Execute update
 	 */
-	function moduleUpdate()
-	{
+	function moduleUpdate() {
 		$oDB = &DB::getInstance();
 		$oModuleModel = getModel('module');
-
 		if(!$oDB->isTableExists('session')) $oDB->createTableByXmlFile($this->module_path.'schemas/session.xml');
-
 		if(!$oDB->isColumnExists("session","cur_mid")) $oDB->addColumn('session',"cur_mid","varchar",128);
-
 		if(!$oDB->isIndexExists("session","idx_session_update_mid")) $oDB->addIndex("session","idx_session_update_mid", array("member_srl","last_update","cur_mid"));
 	}
 
 	/**
 	 * @brief session string decode
 	 */
-	function unSerializeSession($val)
-	{
+	function unSerializeSession($val) {
 		$vars = preg_split('/([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff^|]*)\|/', $val,-1,PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
 		for($i=0; $vars[$i]; $i++) $result[$vars[$i++]] = unserialize($vars[$i]);
 		return $result;
@@ -69,20 +59,12 @@ class session extends ModuleObject
 	/**
 	 * @brief session string encode
 	 */
-	function serializeSession($data)
-	{
+	function serializeSession($data) {
 		if(!count($data)) return;
 
 		$str = '';
 		foreach($data as $key => $val) $str .= $key.'|'.serialize($val);
 		return substr($str, 0, strlen($str)-1).'}';
-	}
-
-	/**
-	 * @brief Re-generate the cache file
-	 */
-	function recompileCache()
-	{
 	}
 }
 /* End of file session.class.php */
