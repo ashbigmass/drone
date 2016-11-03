f1f2c009a0b09ebf11e4bbd8a41419148869ab40
dr11gate <dr11gate@gmail.com>
2016-11-03 13:38:55 +0900

    module_extend.php  삭제했고 db 수정

--- public_html/files/config/db.config.php
+++ public_html/files/config/db.config.php
@@ -1,41 +1,16 @@
 <?php if(!defined("__XE__")) exit();
 $db_info = (object)array (
-  'master_db' => 
-  array (
-    'db_type' => 'mysqli',
-    'db_port' => '3306',
-    'db_hostname' => '192.168.100.12',
-    'db_userid' => 'drcsdb',
-    'db_password' => 'drcsdbwjdqjf',
-    'db_database' => 'drcsdb',
-    'db_table_prefix' => 'drcs_',
+  'master_db' =>
+	  array ('db_type' => 'mysqli', 'db_port' => '3306', 'db_hostname' => '192.168.100.12', 'db_userid' => 'drcsdb', 'db_password' => 'drcsdbwjdqjf', 'db_database' => 'drcsdb', 'db_table_prefix' => 'drcs_',
   ),
-  'slave_db' => 
-  array (
-    0 => 
-    array (
-      'db_type' => 'mysqli',
-      'db_port' => '3306',
-      'db_hostname' => '192.168.100.12',
-      'db_userid' => 'drcsdb',
-      'db_password' => 'drcsdbwjdqjf',
-      'db_database' => 'drcsdb',
-      'db_table_prefix' => 'drcs_',
+  'slave_db' =>
+	  array (0 =>
+		array ('db_type' => 'mysqli', 'db_port' => '3306', 'db_hostname' => '192.168.100.12', 'db_userid' => 'drcsdb', 'db_password' => 'drcsdbwjdqjf', 'db_database' => 'drcsdb', 'db_table_prefix' => 'drcs_',
     ),
   ),
-  'default_url' => 'http://dtcs.saashub.org/',
-  'use_mobile_view' => 'Y',
-  'use_rewrite' => 'N',
-  'time_zone' => '+0900',
-  'use_prepared_statements' => 'Y',
-  'qmail_compatibility' => 'N',
-  'use_db_session' => 'N',
-  'use_ssl' => 'none',
-  'sitelock_whitelist' => 
-  array (
-    0 => '127.0.0.1',
-  ),
-  'use_sso' => 'N',
-  'use_html5' => 'N',
-  'admin_ip_list' => NULL,
+  'default_url' => 'http://dtcs.saashub.org/', 'use_mobile_view' => 'Y', 'use_rewrite' => 'N', 'time_zone' => '+0900', 'use_prepared_statements' => 'Y', 'qmail_compatibility' => 'N', 'use_db_session' => 'N', 'use_ssl' => 'none', 'sitelock_whitelist' =>
+	  array (
+		0 => '127.0.0.1',
+	  ),
+  'use_sso' => 'N', 'use_html5' => 'N', 'admin_ip_list' => NULL,
 );
