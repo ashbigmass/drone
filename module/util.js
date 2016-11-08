9b4008289fd52cec162b5c32aaae847e45cc0b82
totorochief <totorochief@gmail.com>
2016-11-08 18:28:33 +0900

    script, css update commit

--- module/util.js
+++ module/util.js
@@ -1,9 +1,3 @@
-/**
-* Returns an XMLHttp instance to use for asynchronous
-* downloading. This method will never throw an exception, but will
-* return NULL if the browser does not support XmlHttp for any reason.
-* @return {XMLHttpRequest|Null}
-*/
 function createXmlHttpRequest() {
  try {
    if (typeof ActiveXObject != 'undefined') {
@@ -17,13 +11,6 @@ function createXmlHttpRequest() {
  return null;
 };
 
-/**
-* This functions wraps XMLHttpRequest open/send function.
-* It lets you specify a URL and will call the callback if
-* it gets a status code of 200.
-* @param {String} url The URL to retrieve
-* @param {Function} callback The function to call once retrieved.
-*/
 function downloadUrl(url, callback) {
  var status = -1;
  var request = createXmlHttpRequest();
@@ -52,13 +39,6 @@ function downloadUrl(url, callback) {
  }
 };
 
-/**
- * Parses the given XML string and returns the parsed document in a
- * DOM data structure. This function will return an empty DOM node if
- * XML parsing is not supported in this browser.
- * @param {string} str XML string.
- * @return {Element|Document} DOM.
- */
 function xmlParse(str) {
   if (typeof ActiveXObject != 'undefined' && typeof GetObject != 'undefined') {
     var doc = new ActiveXObject('Microsoft.XMLDOM');
