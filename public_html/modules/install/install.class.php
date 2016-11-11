5eefb8ceaf4ebb72999c33c63ec8feb125abe086
lemontreeyk <lemontreeyk@gmail.com>
2016-11-11 10:31:46 +0900

    /modules/install

--- public_html/modules/install/install.class.php
+++ public_html/modules/install/install.class.php
@@ -1,42 +1,16 @@
 <?php
-/* Copyright (C) NAVER <http://www.navercorp.com> */
-/**
- * @class  install
- * @author NAVER (developers@xpressengine.com)
- * @brief install module of the high class
- */
-class install extends ModuleObject
-{
-	/**
-	 * @brief Implement if additional tasks are necessary when installing
-	 */
-	function moduleInstall()
-	{
+class install extends ModuleObject {
+	function moduleInstall() {
 		return new Object();
 	}
 
-	/**
-	 * @brief a method to check if successfully installed
-	 */
-	function checkUpdate()
-	{
+	function checkUpdate() {
 		return false;
 	}
 
-	/**
-	 * @brief Execute update
-	 */
-	function moduleUpdate()
-	{
+	function moduleUpdate() {
 		return new Object();
 	}
 
-	/**
-	 * @brief Re-generate the cache file
-	 */
-	function recompileCache()
-	{
-	}
+	function recompileCache() {	}
 }
-/* End of file install.class.php */
-/* Location: ./modules/install/install.class.php */
