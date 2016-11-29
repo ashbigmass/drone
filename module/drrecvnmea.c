db4808e54b69e843ca7c5efb59f384222134c88c
ashbigmass <ashbigmass@gmail.com>
2016-11-29 16:04:03 +0900

    어후... 힘드네요

--- module/drrecvnmea.c
+++ module/drrecvnmea.c
@@ -1,16 +1,3 @@
-/*
-
-
-gcc drrecvnmea.c -o drrecvnmea -I/usr/include/mysql -L/usr/lib64/mysql -lmysqlclient -lm
-
-   $GPGGA - Global Positioning System Fix Data
-   $GPGSA - GPS DOP and active satellites 
-   $GPGSV - GPS Satellites in view
-   $GPRMC - Recommended minimum specific GPS/Transit data
-   $GPVTG - Track made good and ground speed
-   
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,10 +5,8 @@ gcc drrecvnmea.c -o drrecvnmea -I/usr/include/mysql -L/usr/lib64/mysql -lmysqlcl
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-
 #include "mysql.h"
 
-// Mysql Define
 #define BUFLEN		512
 #define DB_HOST 	"127.0.0.1"
 #define DB_USER 	"dr"
@@ -29,7 +14,6 @@ gcc drrecvnmea.c -o drrecvnmea -I/usr/include/mysql -L/usr/lib64/mysql -lmysqlcl
 #define DB_NAME 	"dr"
 #define DB_PORT		12345
 #define CHOP(x) x[strlen(x) - 1] = ' '
-// UDP Define
 #define PORT 9876
 
 int main(void) {
@@ -69,28 +53,6 @@ int main(void) {
          		
 		
 		printf("receive UDP : %sn", buff_rcv);
-/*      
-		mysql_init(&conn);
-		connection = mysql_real_connect(&conn, DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT, (char *)NULL, 0);
-		if (connection == NULL) {
-			fprintf(stderr, "Mysql connection error : %s", mysql_error(&conn));
-		}
-
-		mysql_query(&conn,"select * from TestDB");
-		sql_result = mysql_store_result(&conn);  // 모든 출력 결과를 서버에서 한번에 다 받아옴(데이터 많을때는 자제하라)
-
-		fields = mysql_num_fields(sql_result); // 필드 갯수 구함
-
-		while(sql_row = mysql_fetch_row(sql_result)) // 한 행을 구함
-		{                                             
-			for (i = 0; i < fields ; i++)  // 모든 필드 값을 출력
-			{
-				printf("%s",sql_row[i]);
-			}
-		}
-		mysql_free_result(sql_result);
-		mysql_close(&conn);
-*/
 	}
 	
 	close(sock);
