f18ec1aaf86538c98eaf4c9923eb9b7dee6436f3
ashbigmass <ashbigmass@gmail.com>
2016-12-05 17:31:36 +0900

    PEAR modified

--- public_html/libs/PEAR.1.9.5/HTTP/Request.php
+++ public_html/libs/PEAR.1.9.5/HTTP/Request.php
@@ -1,55 +1,36 @@
 <?php
 require_once 'HTTP/Request2.php';
 
-class HTTP_Request extends HTTP_Request2
-{
+class HTTP_Request extends HTTP_Request2 {
 	private $reponse = null;
 
-	public function addHeader($name, $value)
-	{
+	public function addHeader($name, $value) {
 		$this->setHeader($name, $value);
 	}
 
-	public function sendRequest($saveBody = true)
-	{
+	public function sendRequest($saveBody = true) {
 		$response = $this->send();
 		$this->response = $response;
 		return $response;
 	}
 
 	public function getResponseCode() {
-		if($this->response)
-		{
-			return $this->response->getStatus();
-		}
+		if($this->response) return $this->response->getStatus();
 	}
 
 	public function getResponseHeader() {
-		if($this->response)
-		{
-			return $this->response->getHeader();	
-		}
+		if($this->response) return $this->response->getHeader();
 	}
 
 	public function getResponseBody() {
-		if($this->response)
-		{
-			return $this->response->getBody();
-		}
+		if($this->response) return $this->response->getBody();
 	}
 
 	public function getResponseCookies() {
-		if($this->response)
-		{
-			return $this->response->getCookies();
-		}
+		if($this->response) return $this->response->getCookies();
 	}
 
-	public function addPostData($name, $value, $preencoded = false)
-	{
+	public function addPostData($name, $value, $preencoded = false) {
 		$this->addPostParameter($name, $value);
 	}
-
 }
-
-?>
